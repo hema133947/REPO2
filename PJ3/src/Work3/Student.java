3b61ca2c14006e9bfe2fd9238334d691bb8562a2
Acer <Acer@192.168.1.62>
2025-03-17 16:33:20 +0530

    add sop

---
+++ PJ3/src/Work3/Student.java
@@ -0,0 +1,23 @@
+package Work3;
+
+public class Student {
+	
+	String name;
+	int age;
+	
+
+
+	public void m1()
+	{
+		System.out.println("HEMA");
+		System.out.println(25);
+		System.out.println("JAGTAP");
+	}
+	
+	public static void main(String[] args) {
+		
+		Student s=new Student();
+		      
+	}
+
+}
